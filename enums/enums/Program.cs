508ebc8a9c899f95f7ef98983c8caff7294692d7
Sigma <sigmw@protonmail.com>
2022-01-31 10:51:06 -0300

    enums

---
+++ enums/enums/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace enums
+{
+    internal class Program
+    {
+        enum Dias
+        {
+            Domingo,
+            Segunda,
+            Terca,
+            Quarta,
+            Quinta,
+            Sexta,
+            Sabado
+        }
+        static void Main(string[] args)
+        {
+            Dias sexta = Dias.Sexta;
+            Dias dias = (Dias)3;
+            Console.WriteLine($"sexta = {sexta}, dia 3 = {dias}");
+            Console.WriteLine($"Número do dia terça: {(int)Dias.Terca}");
+            while (true)
+            {
+                int dia;
+                Console.Write("\nQual o dia da semana você quer? (1-6): ");
+                try
+                {
+                    dia = Convert.ToUInt16(Console.ReadLine());
+                    Console.WriteLine((Dias)dia);
+                } catch
+                {
+                   Console.WriteLine("Ou tá vazio, ou você não colocou um inteiro.");
+                }
+            }
+        }
+    }
+}
