9807786285c0d5b51cc67af0fb1e279651b97816
Sigma <sigmw@protonmail.com>
2022-01-31 11:33:48 -0300

    switch

---
+++ switch/switch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Viagem: Belo Horizonte/MG -> Vitória/ES");
+                    Console.Write("Escolha o transporte [A] - Avião | [B] - Carro | [C] - Ônibus: ");
+                    char transporte = char.Parse(Console.ReadLine().Trim().ToLower());
+                    switch (transporte)
+                    {
+                        case 'a':
+                            tempo = 50;
+                            Console.WriteLine($"Você optou por avião, isso dá exatamente: {tempo} minutos.");
+
+                            break;
+                        case 'b':
+                            tempo = 480;
+                            Console.WriteLine($"Você optou por Carro, isso dá exatamente: {tempo} minutos.");
+
+                            break;
+                        case 'c':
+                            tempo = 660;
+                            Console.WriteLine($"Você optou por Ônibus, isso dá exatamente: {tempo} minutos.");
+
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Opção inválida!");
+                }
+            }
+
+        }
+    }
+}
