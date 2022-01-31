8eb2f4fea97c46793c6b27f4aa91393b4132ae16
Sigma <sigmw@protonmail.com>
2022-01-31 09:16:17 -0300

    const

---
+++ const/const/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace @const
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = Math.PI;
+            Console.WriteLine("{0}", pi.ToString("N4"));
+            Console.ReadKey();
+        }
+    }
+}
