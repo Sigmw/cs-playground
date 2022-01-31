84743a65ba54cd31a5056dca6a3f8545796717a4
Sigma <sigmw@protonmail.com>
2022-01-31 00:55:28 -0300

    beepfuck

---
+++ beepfuck/beepfuck/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beepfuck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Too args!");
+            } else if(args.Length == 0)
+            {
+                Console.WriteLine("You need 1 arg!");
+            }
+            else
+            {
+                try
+                {
+                    int number = int.Parse(args[0]);
+                    for (int i = 0; i < number; i++)
+                    {
+                        Console.Beep();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Not a integer!");
+                }
+            }
+        }
+    }
+}
