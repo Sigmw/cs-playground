e719c5394b2c8e41461ab9362dc229d80b48b9c9
Sigma <sigmw@protonmail.com>
2022-01-31 11:19:52 -0300

    ifs

---
+++ if/if/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Verdadeiro ou falso (false-true): ");
+                try {
+                    bool condicao = Convert.ToBoolean(Console.ReadLine());
+                    if (condicao)
+                    {
+                        Console.WriteLine("Então é verdadeiro.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Então é falso");
+                    }
+                } catch
+                {
+                    Console.WriteLine($"not a boolean! (true-false)");
+                }
+            }
+
+        }
+    }
+}
---
+++ ifnested/ifnested/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifnested
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Digite algo: ");
+                string nome = Console.ReadLine().Trim();
+                if(nome.StartsWith("L"))
+                {
+                    Console.WriteLine("Começa com L!");
+                    if(nome.EndsWith("s"))
+                    {
+                        Console.WriteLine("Termina com s!");
+                        if(nome.Length == 5)
+                        {
+                            Console.WriteLine("Possui 5 letras.");
+                        }
+                    }
+                } else if(nome.StartsWith("R"))
+                {
+                    Console.WriteLine("Começa com R!");
+                    if(nome.EndsWith("o"))
+                    {
+                        Console.WriteLine("Termina com o!");
+                        if(nome.Length == 7)
+                        {
+                            Console.WriteLine("Possui 7 letras!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
