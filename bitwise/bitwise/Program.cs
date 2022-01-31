5c70260d1cdbee37bc54c0a7337db7e7a47ec3e3
Sigma <sigmw@protonmail.com>
2022-01-31 10:31:37 -0300

    bitwise

---
+++ bitwise/bitwise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitwise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // >> deslocamento de bits para a direita (dobra)
+            // << deslocamento de bits para a esquerda (diminui pela metade)
+            int num = 10;
+            num <<= 4; // 20 40 80 160
+            Console.WriteLine(num);
+            num = 20;
+            num >>= 2; // 10 5
+            Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
