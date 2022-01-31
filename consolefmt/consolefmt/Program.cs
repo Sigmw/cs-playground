67308559ad4269c184234da14bbcc81659121768
Sigma <sigmw@protonmail.com>
2022-01-31 08:59:48 -0300

    consolefmt

---
+++ consolefmt/consolefmt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace consolefmt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+            n1 = 10; n2 = 20; n3 = 30;
+            Console.Write("n1 = {0}\nn2 = {1}\nn3 = {2}\n4 = {3}\n", n1, n2, n3, 4);
+            double fmt = 5.50;
+            Console.WriteLine("{0,800}", fmt); // "800" indica o tamanho do espaçamento
+            Console.WriteLine("{0,15:c}", fmt); // :c coloca o R$
+            Console.WriteLine("{0,15:p}", fmt); // porcentagem
+            Console.ReadKey();
+        }
+    }
+}
--- operators/operators/Program.cs
+++ operators/operators/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace operators
 {
@@ -10,9 +6,18 @@ namespace operators
     {
         static void Main(string[] args)
         {
-            int res = 10 + 5;
+            int res = 10 + 5 * 2;
+            bool varmenorque5 = res < 5;
+            int num = 1;
+            num++;//num += 1; num = num + 1;
+            num *= 2;
+            bool op = (5 > 3) && (1 > 3);
+            Console.WriteLine(num);
             Console.WriteLine(res);
+            Console.WriteLine(varmenorque5);
+            Console.WriteLine(op);
             Console.ReadKey();
+
         }
     }
 }
