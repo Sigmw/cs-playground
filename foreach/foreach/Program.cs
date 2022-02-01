2a83690c0df6209c1c7201ec713ce7d4183693fd
Sigma <sigmw@protonmail.com>
2022-01-31 22:11:36 -0300

    statements

---
+++ foreach/foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[3]{11, 12, 13};
+            foreach(int numero in num)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ while/while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (num <= 10)
+            {
+                num++;
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
